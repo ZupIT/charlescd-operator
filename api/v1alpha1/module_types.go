d2488eb8d5c67ef12c790636790b9b8a50436c8f
Tiago Angelo <tiago.angelo@zup.com.br>
2021-12-27 10:53:57 -0300

    Create a new API and Controller for Module deployments

---
+++ api/v1alpha1/module_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ModuleSpec defines the desired state of Module
+type ModuleSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Module. Edit module_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ModuleStatus defines the observed state of Module
+type ModuleStatus struct { // INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Module is the Schema for the modules API
+type Module struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ModuleSpec   `json:"spec,omitempty"`
+	Status ModuleStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ModuleList contains a list of Module
+type ModuleList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Module `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Module{}, &ModuleList{})
+}
